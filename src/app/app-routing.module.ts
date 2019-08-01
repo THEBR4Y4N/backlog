6b07e526c78ed4007b9bbf72da9cbd1671468087
unknown <garzonbrayan1201@hotmail.com>
2019-08-01 15:20:24 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { Page404Component } from './components/page404/page404.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:HomepageComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'tareas',component:TareasComponent},
+  {path:'register',component:RegisterPageComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'login',component:LoginPageComponent},
+  {path:'**',component:Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +15,18 @@ import {environment} from '../environments/environment';
 import { TareasComponent } from './components/tareas/tareas.component';
 import { TareasListComponent } from './components/tareas/tareas-list/tareas-list.component';
 import { TareaComponent } from './components/tareas/tarea/tarea.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
 //services
 import { TareaService } from './services/tarea.service';
-import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
@@ -24,14 +34,24 @@ import { NabvarComponent } from './components/nabvar/nabvar.component';
     TareasComponent,
     TareasListComponent,
     TareaComponent,
-    NabvarComponent
+    NabvarComponent,
+    HomepageComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    DashboardComponent,
+    Page404Component,
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule
   ],
   providers: [TareaService],
   bootstrap: [AppComponent]
