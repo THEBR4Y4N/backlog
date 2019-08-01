9348cd7f1dc938cab67bf52d366d3200aa889a20
unknown <garzonbrayan1201@hotmail.com>
2019-08-01 16:03:29 -0500

    register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from '@angular/forms';
-import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 //firebase
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireModule} from 'angularfire2';
-import {environment} from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 //components
 import { TareasComponent } from './components/tareas/tareas.component';
 import { TareasListComponent } from './components/tareas/tareas-list/tareas-list.component';
 import { TareaComponent } from './components/tareas/tarea/tarea.component';
 import { NabvarComponent } from './components/nabvar/nabvar.component';
-//services
-import { TareaService } from './services/tarea.service';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
@@ -25,6 +24,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { Page404Component } from './components/page404/page404.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { FooterComponent } from './components/footer/footer.component';
+//services
+import { TareaService } from './services/tarea.service';
+import { AuthService } from './services/auth.service';
 
 
 
@@ -51,9 +53,10 @@ import { FooterComponent } from './components/footer/footer.component';
     FormsModule,
     NgbModule,
     NgbPaginationModule,
-    NgbAlertModule
+    NgbAlertModule,
+    AngularFireAuthModule
   ],
-  providers: [TareaService],
+  providers: [TareaService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router:Router
+  ) { }
+  public email: string = '';
+  public password: string = '';
 
   ngOnInit() {
+  }
+
+  onSubmitAddUser() {
+    this.authService.RegisterUser(this.email,this.password)
+    .then((res)=>{
+      console.log('Usuario creado con exito');
+      console.log(res);
+      this.router.navigate(['/dashboard']);
+    }).catch((err)=>{
+      console.log('Algo salio mal :(');
+      console.log(err);
+    });
   }
 
 }
